ded2b832da88b568980f50c3a643110612135f4b
kwamecody <kwamecody@gmail.com>
2025-09-18 19:21:09 +0000

    data models for chat

--- messaging_app/messaging_app/chats/models.py
+++ messaging_app/messaging_app/chats/models.py
@@ -1,3 +1,61 @@
+import uuid
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+
+    ROLE_CHOICES = [
+        ('guest', 'Guest'),
+        ('host', 'Host'),
+        ('admin', 'Admin'),
+    ]
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='guest')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # Remove username if you prefer email as the login field - might remove later
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "first_name", "last_name"]
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.role})"
+
+
+
+class Message(models.Model):
+    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    sender_id = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="messages"
+    )
+    recipient_id = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="received_messages"
+    )
+
+    message_body = models.TextField(null=False)
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.sender.email}: {self.message_body[:30]}..."
+
+
+class Conversation(models.Model):
+    conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    participants_id = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="conversations"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        participant_emails = ", ".join([user.email for user in self.participants.all()])
+        return f"Conversation {self.conversation_id} between {participant_emails}"
