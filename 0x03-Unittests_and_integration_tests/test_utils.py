194a0149a398743a6c121e7504612c3267aa38a1
kwamecody <kwamecody@gmail.com>
2025-10-01 20:07:38 +0000

    test the get_json function, mocking external HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 class TestAccessNestedMap(unittest.TestCase):
     """Test the access_nested_map function from utils"""
@@ -26,4 +27,22 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_path, path)
 
     
-    
+
+class TestGetJson(unittest.TestCase):
+    """
+    Tests the get_json function from utils, mocking external HTTP calls.
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch('requests.get')
+    def test_get_json(self, test_url, test_payload,mock_get):
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        result = get_json(test_url)
+        
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result,test_payload)
