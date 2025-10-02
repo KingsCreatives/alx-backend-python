3ce173ad0c43f326db1fb12e61b4745b3c5606ea
kwamecody <kwamecody@gmail.com>
2025-10-02 08:03:27 +0000

    Tests that GithubOrgClient.has_license returns the correct boolean value.

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -78,4 +78,24 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_public_repos_url.assert_called_once()
 
         mock_get_json.assert_called_once()
+    
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+        ({}, "my_license", False),
+        ({"license": None}, "my_license", False),
+    ])
+    def test_has_license(
+        self,
+        repo: dict,
+        license_key: str,
+        expected: bool
+    ) -> None:
+        """
+        Tests that GithubOrgClient.has_license returns the correct boolean value.
+        """
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
+        
         
