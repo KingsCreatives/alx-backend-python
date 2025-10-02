48e973f3dddfbcc6916c2b57425fa9a26b3dcfcf
kwamecody <kwamecody@gmail.com>
2025-10-02 07:30:12 +0000

    Tests that _public_repos_url returns the correct URL based on the         mocked org property payload

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -24,3 +24,23 @@ class TestGithubOrgClient(unittest.TestCase):
 
         expected_url = f"https://api.github.com/orgs/{org_name}"
         mock_get_json.assert_called_once_with(expected_url)
+    
+
+    def test_public_repos_url(self) -> None:
+        
+        mock_payload = {
+            "repos_url": "https://api.github.com/orgs/google/repos"
+        }
+
+        with patch(
+            'client.GithubOrgClient.org',
+            new_callable=PropertyMock
+        ) as mock_org:
+            mock_org.return_value = mock_payload
+
+            client = GithubOrgClient("google")
+            result = client._public_repos_url
+
+            self.assertEqual(result, mock_payload["repos_url"])
+
+            mock_org.assert_called_once()
