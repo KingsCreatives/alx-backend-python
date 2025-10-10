c87ed81a9abf4a4dfc340ed6b044f121e48eee24
kwamecody <kwamecody@gmail.com>
2025-10-10 12:34:22 +0000

    custom manager for unread messages

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -5,6 +5,21 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 # Create your models here.
+class UnreadMessagesManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()
+    
+    def unread(self,user):
+        return self.filter(receiver=user, read=False)
+    
+    def unread_min_fields(self,user):
+        return self.unread(user).only(
+            'id',
+            'sender_id',
+            'content',
+            'timestamp'
+        )
+    
 class Message(models.Model):
     sender = models.ForeignKey(User, related_name='messaging_sender', on_delete=models.CASCADE)
     receiver = models.ForeignKey(User, name='messaging_receiver', on_delete=models.CASCADE)
@@ -19,13 +34,15 @@ class Message(models.Model):
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     edited = models.BooleanField(default=False)
+    read = models.BooleanField(default=False)
+    objects = models.Manager()
+    unread_messages = UnreadMessagesManager()
 
     class Meta:
         ordering = ['-timestamp']
 
     def __str__(self):
         return f"From {self.sender.username} to {self.receiver.username}"
-
 
 class Notification(models.Model):
     user = models.ForeignKey(User, related_name='messaging_notifications', on_delete=models.CASCADE)
